8bbe5393213b21aabc43cd71cf809887844d530e
codri5 <97296166+codri5@users.noreply.github.com>
2023-02-20 18:45:21 +0000

    added questions

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    'Please enter the title of your project: ',
+    'Please enter a description: ',
+    'Please enter installation instructions: ',
+    'Please enter usage information: ',
+    'Please select the license used: ',
+    'Please enter contribution guidelines: ',
+    'Please enter test instructions: ',
+    'Please enter your GitHub username: ',
+    'Please enter your email address: '
 ];
 
 // function to write README file
