9cac429f1d6c613b69f66372b38e6e1d73fd98ff
codri5 <97296166+codri5@users.noreply.github.com>
2023-02-20 19:03:12 +0000

    updated generate markdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,14 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+          ## ${data.description}
+          ## ${data.contents}
+          ## ${data.installation}
+          ## ${data.usage}
+          ## ${data.license}
+          ## ${data.contribute}
+          ## ${data.tests}
+          ## ${data.questions}
 `;
 }
 
