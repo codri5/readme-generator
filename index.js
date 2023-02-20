f07d7023718d87f3054485502191110ba6edec6e
codri5 <97296166+codri5@users.noreply.github.com>
2023-02-20 19:47:36 +0000

    updated questions array

--- index.js
+++ index.js
@@ -5,15 +5,54 @@ const generateMarkdown = require('./utils/generateMarkdown');
 
 // array of questions for user
 const questions = [
-    'Please enter the title of your project: ',
-    'Please enter a description: ',
-    'Please enter installation instructions: ',
-    'Please enter usage information: ',
-    'Please select the license used: ',
-    'Please enter contribution guidelines: ',
-    'Please enter test instructions: ',
-    'Please enter your GitHub username: ',
-    'Please enter your email address: '
+    { type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?'
+    },
+
+    { type: 'input',
+      name: 'description',
+      message: 'Please enter a description: '
+    },
+
+    { type: 'input',
+      name: 'installation',
+      message: 'Please enter installation instructions: '
+    },
+
+    { type: 'input',
+      name: 'usage',
+      message: 'Please enter usage information: '
+    },
+
+    { type: 'list',
+      name: 'license',
+      message: 'Please select the license used: ',
+      choices: ['Apache License 2.0', 'GNU GPL v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License',
+                'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal',
+                'Eclipse Public License 2.0', 'GNU AGPL v3.0', 'GNU GPL v2.0', 'GNU LGPL v2.1', 'Mozilla Public License 2.0',
+                'The Unlicense']
+    },
+
+    { type: 'input',
+      name: 'contribute',
+      message: 'Please enter contribution guidelines: ',
+    },
+
+    { type: 'input',
+      name: 'test',
+      message: 'Please enter testing instructions: ',
+    },
+
+    { type: 'input',
+      name: 'username',
+      message: 'What is your GitHub username? ',
+    },
+
+    { type: 'input',
+      name: 'email',
+      message: 'What is your email address? ',
+    }
 ];
 
 // function to write README file
@@ -22,7 +61,7 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    const userInput = inquirer.prompt(questions);
 }
 
 // function call to initialize program
